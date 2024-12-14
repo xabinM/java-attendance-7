f79346c009deac706db70434840ca1c69c683323
Min <binine2@gmail.com>
2024-12-14 16:39:29 +0900

    feat: view단 입출력 기능 구현

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -1,14 +1,57 @@
 package attendance.view;
 
+import attendance.repository.CrewNameRepository;
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
 
 public class InputView {
     public static String requestFunctionNumber() {
-        return Console.readLine().trim();
+        String input =  Console.readLine().trim();
+        validateFunctionNumber(input);
+        return input;
+    }
+
+    public static String requestNickname() {
+        String input = Console.readLine();
+        validateRegisteredName(input);
+        return input;
+    }
+
+    public static String requestTime() {
+        String input = Console.readLine();
+        String[] temp = input.split(":");
+        validateTimeAndMinute(temp);
+        return input;
+    }
+
+    private static void validateTimeAndMinute(String[] temp) {
+        int time = parseInteger(temp[0]);
+        int minute = parseInteger(temp[1]);
+        if (time > 24 || time < 0) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식을 입력하였습니다.");
+        }
+        if (minute > 60 || minute < 0) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식을 입력하였습니다.");
+        }
+    }
+
+    private static void validateRegisteredName(String input) {
+        if (!CrewNameRepository.getNames().contains(input)) {
+            throw new IllegalArgumentException("[ERROR] 등록되지 않은 닉네임입니다.");
+        }
+    }
+
+    private static void validateFunctionNumber(String input) {
+        List<String> temp = List.of("1", "2", "3", "4", "Q");
+
+        if (!temp.contains(input)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식을 입력하였습니다.");
+        }
     }
 
 
-    private int parseInteger(String input) {
+    private static int parseInteger(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
--- src/main/java/attendance/view/OutputView.java
+++ src/main/java/attendance/view/OutputView.java
@@ -6,7 +6,7 @@ import camp.nextstep.edu.missionutils.DateTimes;
 public class OutputView {
 
     public static void printTodayAndRequestChoiceFunction() {
-        System.out.printf("오늘은 %d월 %d일 %s입니다. 기능을 선택해주세요.",
+        System.out.printf("오늘은 %d월 %d일 %s입니다. 기능을 선택해주세요.\n",
                 DateTimes.now().getMonthValue(), DateTimes.now().getDayOfMonth(),
                 DayOfWeek.getDayOfWeek(DateTimes.now().getDayOfWeek().getValue()));
     }
@@ -17,5 +17,26 @@ public class OutputView {
         System.out.println("3. 크루별 출석 기록 확인");
         System.out.println("4. 제적 위험자 확인");
         System.out.println("Q. 종료");
+    }
+
+    public static void requestNickname() {
+        System.out.println("닉네임을 입력해 주세요.");
+    }
+
+    public static void requestTime() {
+        System.out.println("등교 시간을 입력해 주세요.");
+    }
+
+    public static void printAttendanceInfo() {
+        System.out.println();
+    }
+
+    public static void validateHoliday() {
+        String errorMessage = String.format("[ERROR] %d월 %d일 %s은 등교일이 아닙니다.", DateTimes.now().getMonthValue(), DateTimes.now().getDayOfMonth(),
+                DayOfWeek.getDayOfWeek(DateTimes.now().getDayOfWeek().getValue()));
+        if (DayOfWeek.getDayOfWeek(DateTimes.now().getDayOfWeek().getValue()).equals("토요일") ||
+                DayOfWeek.getDayOfWeek(DateTimes.now().getDayOfWeek().getValue()).equals("일요일")) {
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 }
